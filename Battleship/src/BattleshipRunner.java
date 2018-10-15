0dd77e0d6cc7ca30a339bebd3c308f8349d1fefa
alina.antillon <alina.antillon@MU-C1-19.mullen.pvt.k12.co.us>
2018-10-15 09:09:51 -0600

    row and column user input

--- Battleship/src/BattleshipRunner.java
+++ Battleship/src/BattleshipRunner.java
@@ -6,41 +6,14 @@ public class BattleshipRunner
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
-				Scanner userinput = new Scanner(System.in);
-				System.out.println("Would you like to play a game?(Yes or no)");
-				String decision = userinput.nextLine();
-				if (decision.equals("no"))
-					{
-						System.out.println("Okay! Have a good day!");
-					}
-				else
-					{
-						System.out.println("The game you will be playing today will be Battleship. Do you know how to play?");
-						Scanner userinput2 = new Scanner(System.in);
-						String answer = userinput.nextLine();
-						if (answer.equals("no"))
-							{
-								System.out.println("You might want to learn before playing");
-							}
-						else
-							{
-								System.out.println("Okay then let's play!");
-								Scanner userinput3 = new Scanner(System.in);
-								System.out.println("What is the row you want to attack?");
-								rowInput = move.substring(0,1)
-										if (rowInput.equals("A"))|| (rowInput.equals("a"))
-											{
-												row = 0;
-											}
-								Scanner userinput = new Scanner(System.in);
-								System.out.println("What is the column you want to attack?");
-								
 								Board.createBoard();
 								Board.display();
-							}
-					}
-				
-
 			}
+		
+					
+					
+				/////ships - 3, 2, 2, 1
+
+			
 
 	}
